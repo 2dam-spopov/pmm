8d2561902b497355232dcac2b6a82d917c50590f
2dam-spopov <2dam.spopov@institutoserpis.org>
2018-09-20 13:54:06 +0200

    Act

--- MyApplication/app/src/main/java/com/example/serpop/myapplication/Main2Activity.java
+++ MyApplication/app/src/main/java/com/example/serpop/myapplication/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example.serpop.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final TextView otroSaludo=(TextView)findViewById(R.id.miLbl);
+        Bundle miBundleRecoger=getIntent().getExtras();
+        otroSaludo.setText(miBundleRecoger.getString("TEXTO"));
     }
 }
--- MyApplication/app/src/main/java/com/example/serpop/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/serpop/myapplication/MainActivity.java
@@ -17,16 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final EditText miTexto = (EditText) findViewById(R.id.miTxt);
         final Button miBoton = (Button) findViewById(R.id.miBtn);
         final TextView elSaludo = (TextView) findViewById(R.id.miLbl);
+
         miBoton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String mensajePaso = "Te saludo " + miTexto.getText();
                 elSaludo.setText(mensajePaso);
 
-                Intent mio=new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(mio);
+                Intent miIntent=new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(miIntent);
             }
         });
 
